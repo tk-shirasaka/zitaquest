0884ba21de0336d761cf77228d049278c9b8960d
shirasaka <shirasaka@n-create.co.jp>
2020-12-20 13:58:03 +0900

    使用するテーブル作成

---
+++ database/migrations/2020_12_20_044238_create_quests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quests', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('question_id')->references('id')->on('questions');
+            $table->tinyInteger('no');
+            $table->string('place', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quests');
+    }
+}
